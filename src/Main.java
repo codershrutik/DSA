90856d5b58aebaa7a4c0407d12656c15849e9f55
ShrutiKhandelwal <shrutikhandelwal@fico.com>
2026-02-12 11:25:29 +0530

    Linked List - Append

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,12 @@ import LinkedList.LinkedList;
 public class Main
 {
     public static void main(String[] args) {
-        LinkedList myLinkedList = new LinkedList(4);
+        LinkedList myLinkedList = new LinkedList(1);
+        myLinkedList.append(2);
 
-        myLinkedList.getHead();
-        myLinkedList.getTail();
-        myLinkedList.getHead();
+//        myLinkedList.getHead();
+//        myLinkedList.getTail();
+//        myLinkedList.getHead();
 
         myLinkedList.printList();
     }
