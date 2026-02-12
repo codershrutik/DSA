8156e557dc5e615e31716efead43d20c99f89127
ShrutiKhandelwal <shrutikhandelwal@fico.com>
2026-02-12 11:11:35 +0530

    Linked List - Print List

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
 import LinkedList.LinkedList;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main
 {
     public static void main(String[] args) {
         LinkedList myLinkedList = new LinkedList(4);
+
+        myLinkedList.getHead();
+        myLinkedList.getTail();
+        myLinkedList.getHead();
+
+        myLinkedList.printList();
     }
 }
