96f78b1cf1cccd26dd8417ad8006dfd533fc8b6b
ShrutiKhandelwal <shrutikhandelwal@fico.com>
2026-02-12 12:11:49 +0530

    Linked List - Prepend

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,8 @@ public class Main
     public static void main(String[] args) {
         LinkedList myLinkedList = new LinkedList(1);
         myLinkedList.append(2);
+        myLinkedList.prepend(0);
 
-        // Returns Node 2
-        System.out.println(myLinkedList.removeLast());
-        // Returns Node 1
-        System.out.println(myLinkedList.removeLast());
-        // Returns null
-        System.out.println(myLinkedList.removeLast());
+        myLinkedList.printList();
     }
 }
