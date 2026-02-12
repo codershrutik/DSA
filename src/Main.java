78c3c6a834059d63d738eb462a1bc2c085c9c23c
ShrutiKhandelwal <shrutikhandelwal@fico.com>
2026-02-12 12:47:36 +0530

    Linked List - Insert

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,12 @@ import LinkedList.LinkedList;
 public class Main
 {
     public static void main(String[] args) {
-        LinkedList myLinkedList = new LinkedList(1);
+        LinkedList myLinkedList = new LinkedList(0);
         myLinkedList.append(2);
-        myLinkedList.append(3);
-        myLinkedList.prepend(0);
 
-        myLinkedList.set(2,4);
+        myLinkedList.printList();
 
+        myLinkedList.insert(1,1);
         myLinkedList.printList();
     }
 }
